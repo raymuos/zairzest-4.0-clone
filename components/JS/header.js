950cb907142359472db043942fb61ce7ccaf26da
raymuos <soumyarjrout7@gmail.com>
2025-05-12 13:17:59 +0530

    fixing pages attempt 2

--- components/JS/header.js
+++ components/JS/header.js
@@ -1,4 +1,17 @@
-const base = window.location.origin /*+ "/zairzest-4.0-clone"*/
+function getBasePath() {
+  const pathSegments = window.location.pathname.split('/');
+  const repoName = pathSegments[1]; // assumes /RepoName/...
+  
+  // If hosted on GitHub Pages, use repo name
+  if (window.location.hostname.includes("github.io") && repoName) {
+    return `/${repoName}`;
+  }
+
+  // Else, local or root deployment
+  return "";
+}
+
+const base =  getBasePath();
 
 export function Header(){
 
