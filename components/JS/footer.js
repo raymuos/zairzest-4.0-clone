2bc7e4cbebc94f63e48d1453b1ebee915761a8c7
raymuos <soumyarjrout7@gmail.com>
2025-05-12 13:49:58 +0530

    fixing pages attempt 3

--- components/JS/footer.js
+++ components/JS/footer.js
@@ -1,3 +1,17 @@
+function getBasePath() {
+  const pathSegments = window.location.pathname.split('/');
+  const repoName = pathSegments[1]; // assumes /RepoName/...
+  
+  // If hosted on GitHub Pages, use repo name
+  if (window.location.hostname.includes("github.io") && repoName) {
+    return `/${repoName}`;
+  }
+
+  // Else, local or root deployment
+  return "";
+}
+
+
 export function Footer(){
 
     return `    
--- components/JS/header.js
+++ components/JS/header.js
@@ -19,9 +19,9 @@ export function Header(){
     <header>
         <p style="cursor: pointer;" onclick="location.href='${base}/index.html'"><span id="zairzest-span">ZAIRZEST </span> <span id="span-4-0"> 4.0</span></p>
         <div id="menu-div">
-            <a href="${base}/Pages/about.html">About Us</a>
-            <a href="${base}/Pages/register.html">Register</a>
-            <a href="${base}/Pages/login.html">Sign In</a>
+            <a href="${base}/Pages/About.html">About Us</a>
+            <a href="${base}/Pages/Register.html">Register</a>
+            <a href="${base}/Pages/Login.html">Sign In</a>
         </div>
     </header>
     `
