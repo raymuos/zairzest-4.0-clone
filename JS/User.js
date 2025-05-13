5fe781768859f77bd45e8562b9ce5df8be8eedfb
raymuos <soumyarjrout7@gmail.com>
2025-05-13 23:13:11 +0530

    fixed header links

--- JS/User.js
+++ JS/User.js
@@ -1,5 +1,8 @@
-import {Header} from "../components/JS/header.js";
+import {Header, showSidebar, hideSidebar} from "../components/JS/header.js";
 import {Footer} from "../components/JS/footer.js";
 
 document.body.innerHTML += Header();
 document.body.innerHTML += Footer();
+
+document.getElementById("menu-bar-icon").onclick = () => showSidebar();
+document.getElementById("close-menu-icon").onclick = () => hideSidebar();
