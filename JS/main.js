9298d7d2b1b2502fa003a93b00a476429bf9d9c8
raymuos <soumyarjrout7@gmail.com>
2025-05-13 19:33:49 +0530

    added menu bar icon for smaller devices

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,10 @@
-import {Header} from "../components/JS/header.js";
+import {Header, showSidebar, hideSidebar} from "../components/JS/header.js";
 import {Footer} from "../components/JS/footer.js";
 import {EventsForYou} from "../components/JS/eventsForYou.js";
 
 document.body.innerHTML += Header();
 document.body.innerHTML += EventsForYou();
 document.body.innerHTML += Footer();
+
+document.getElementById("menu-bar-icon").onclick = () => showSidebar();
+document.getElementById("close-menu-icon").onclick = () => hideSidebar();
--- components/JS/header.js
+++ components/JS/header.js
@@ -11,18 +11,47 @@ function getBasePath() {
   return "";
 }
 
+export function showSidebar(){
+  document.getElementById("menu-sidebar").style.display = "flex";
+}
+
+export function hideSidebar(){
+  document.getElementById("menu-sidebar").style.display = "none";
+}
+
 const base =  getBasePath();
 
 export function Header(){
 
     return `
-    <header>
-        <p style="cursor: pointer;" onclick="location.href='${base}/index.html'"><span id="zairzest-span">ZAIRZEST </span> <span id="span-4-0"> 4.0</span></p>
+        <header>
+        <a href="/index.html">
+            <span id="zairzest-span">ZAIRZEST </span> <span id="span-4-0"> 4.0</span>
+        </a>
         <div id="menu-div">
-            <a href="${base}/Pages/About.html">About Us</a>
-            <a href="${base}/Pages/Register.html">Register</a>
-            <a href="${base}/Pages/Login.html">Sign In</a>
+            <a href="/Pages/About.html">About Us</a>
+            <a href="/Pages/Register.html">Register</a>
+            <a href="/Pages/Login.html">Sign In</a>
         </div>
+
+        <p id="menu-bar-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#fff">
+                <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/>
+            </svg>
+        </p>
+
+        <div id="menu-sidebar">
+            <p id="close-menu-icon">
+                <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#fff">
+                    <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/>
+                </svg>
+            </p>
+            <a href="/Pages/About.html">About Us</a>
+            <a href="/Pages/Register.html">Register</a>
+            <a href="/Pages/Login.html">Sign In</a>
+        </div>
+
     </header>
+
     `
 }
