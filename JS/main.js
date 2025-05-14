ccfaaa1b6451f5610cc64b1bd717ea1fe6dfa3a7
raymuos <soumyarjrout7@gmail.com>
2025-05-14 22:34:50 +0530

    Added final animations for index page

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,39 @@
 import {Header, showSidebar, hideSidebar} from "../components/JS/header.js";
 import {Footer} from "../components/JS/footer.js";
 
+
 document.addEventListener('DOMContentLoaded', () => {
     const spinners = document.querySelectorAll(".spin");
+    const observer = new IntersectionObserver( (entries, obs) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting){
+                entry.target.classList.add('animate');
+                obs.unobserve(entry.target);
+            }
+        })
+    
+    })
+    spinners.forEach( elemento => observer.observe(elemento));
+    
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const spinners = document.querySelectorAll(".slideX");
+    const observer = new IntersectionObserver( (entries, obs) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting){
+                entry.target.classList.add('animate');
+                obs.unobserve(entry.target);
+            }
+        })
+    
+    })
+    spinners.forEach( elemento => observer.observe(elemento));
+    
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const spinners = document.querySelectorAll(".slideY");
     const observer = new IntersectionObserver( (entries, obs) => {
         entries.forEach((entry) => {
             if (entry.isIntersecting){
