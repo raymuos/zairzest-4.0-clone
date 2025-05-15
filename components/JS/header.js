2f29dce16cc52f4eb0b894b16a5e7f26e8e9e1e0
raymuos <soumyarjrout7@gmail.com>
2025-05-16 00:53:23 +0530

    Index Page is responsive now

--- components/JS/header.js
+++ components/JS/header.js
@@ -41,7 +41,7 @@ export function Header(){
             </svg>
         </p>
 
-        <div id="menu-sidebar">
+        <div id="menu-sidebar" class="slideX">
             <p id="close-menu-icon">
                 <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#fff">
                     <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/>
