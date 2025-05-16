1ca718b38688fa83e1cded4d604a46aec72f18b7
Mohan Reddy <mohanre31@gmail.com>
2025-05-16 20:46:36 +0530

    random testing

--- components/JS/header.js
+++ components/JS/header.js
@@ -22,37 +22,54 @@ export function hideSidebar(){
 const base =  getBasePath();
 
 
-export function Header(){
+export function Header() {
+  return `
+    <header>
+      <a class="load-link" href="${base}/index.html">
+        <span id="zairzest-span">ZAIRZEST </span> <span id="span-4-0"> 4.0</span>
+      </a>
+      <div id="menu-div">
+        <a class="load-link" href="${base}/Pages/About.html">About Us</a>
+        <a class="load-link" href="${base}/Pages/Register.html">Register</a>
+        <a class="load-link" href="${base}/Pages/Login.html">Sign In</a>
+      </div>
 
-    return `
-        <header>
-        <a href="/index.html">
-            <span id="zairzest-span">ZAIRZEST </span> <span id="span-4-0"> 4.0</span>
-        </a>
-        <div id="menu-div">
-            <a href="${base}/Pages/About.html">About Us</a>
-            <a href="${base}/Pages/Register.html">Register</a>
-            <a href="${base}/Pages/Login.html">Sign In</a>
-        </div>
+      <p id="menu-bar-icon">
+        <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#fff">
+          <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/>
+        </svg>
+      </p>
 
-        <p id="menu-bar-icon">
-            <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#fff">
-                <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/>
-            </svg>
+      <div id="menu-sidebar">
+        <p id="close-menu-icon">
+          <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#fff">
+            <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/>
+          </svg>
         </p>
-
-        <div id="menu-sidebar">
-            <p id="close-menu-icon">
-                <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#fff">
-                    <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/>
-                </svg>
-            </p>
-            <a href="${base}/Pages/About.html">About Us</a>
-            <a href="${base}/Pages/Register.html">Register</a>
-            <a href="${base}/Pages/Login.html">Sign In</a>
-        </div>
-
+        <a class="load-link" href="${base}/Pages/About.html">About Us</a>
+        <a class="load-link" href="${base}/Pages/Register.html">Register</a>
+        <a class="load-link" href="${base}/Pages/Login.html">Sign In</a>
+      </div>
     </header>
+    <div id="page-loader" style="display: none;">
+      <div class="loader"></div>
+    </div>
+    <script>
+      document.querySelectorAll(".load-link").forEach(link => {
+        link.addEventListener("click", function (e) {
+          e.preventDefault();
+          const href = this.getAttribute("href");
 
-    `
+          // Show the loader
+          const loader = document.getElementById("page-loader");
+          loader.style.display = "flex";
+
+          // Delay navigation
+          setTimeout(() => {
+            window.location.href = href;
+          }, 1200); // matched with loader transition
+        });
+      });
+    </script>
+  `;
 }
