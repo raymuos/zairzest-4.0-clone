833053b9f5777d7be5656120c40a40fd922313a3
raymuos <soumyarjrout7@gmail.com>
2025-05-16 09:47:28 +0530

    index responsive in mobiles now!!

--- components/JS/header.js
+++ components/JS/header.js
@@ -36,14 +36,14 @@ export function Header(){
         </div>
 
         <p id="menu-bar-icon">
-            <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#fff">
+            <svg class="hamburger" xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#fff">
                 <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/>
             </svg>
         </p>
 
         <div id="menu-sidebar" class="slideX">
             <p id="close-menu-icon">
-                <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#fff">
+                <svg class="cross" xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#fff">
                     <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/>
                 </svg>
             </p>
